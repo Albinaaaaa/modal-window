3517a705d3ff9ad4e31c60f90927dd0e64e0665f
Albina <albinarubanchuk@gmail.com>
2023-01-13 23:02:41 +0200

    add polyfil for matches and closest

--- script.js
+++ script.js
@@ -99,3 +99,33 @@ document.addEventListener("keydown", function (e) {
     popupClose(popupActive);
   }
 });
+
+// add polyfils
+
+(function () {
+  // проверяем поддержку
+  if (!Element.prototype.closest) {
+    // реализуем
+    Element.prototype.closest = function (css) {
+      var node = this;
+
+      while (node) {
+        if (node.matches(css)) return node;
+        else node = node.parentElement;
+      }
+      return null;
+    };
+  }
+})();
+
+(function () {
+  // проверяем поддержку
+  if (!Element.prototype.matches) {
+    // определяем свойство
+    Element.prototype.matches =
+      Element.prototype.matchesSelector ||
+      Element.prototype.webkitMatchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector;
+  }
+})();
